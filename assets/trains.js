b73a0ed767a4d78cb3a0e3734d260b3d930451c6
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-06-30 15:37:44 -0400

    with a lot of Dean's help, the JS is fixed!

--- assets/trains.js
+++ assets/trains.js
@@ -1,5 +1,5 @@
 var config = {
-  // how do I protect the apiKey?
+  // how do i protect apiKey?
   apiKey: "AIzaSyDaY_v5Av_DAEFUkeELOvs2xBWZsR8qAvw",
   authDomain: "trains-rushjob.firebaseapp.com",
   databaseURL: "https://trains-rushjob.firebaseio.com",
@@ -9,13 +9,18 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
 var database = firebase.database();
 
 var trainName = "";
 var trainDestination = "";
-var trainStarts = null;
-var trainHowLongUntil = null;
+var trainStarts = "";
+var trainHowLongUntil = "";
+var nextTrain = null; 
+var preordained = null;
+var trainNext = null;
+
+
+
 
 $("#addTrain").on("click", function(event) {
   event.preventDefault();
@@ -48,54 +53,57 @@ $("#addTrain").on("click", function(event) {
   // $("#primero").empty();
   // $("#howOften").empty();
 
-  database.ref().on("child_added", function(childSnapshot) {
-    console.log(childShanpshot.val());
+  
 
-    var trainName = childSnapshot.val().name;
-    var trainDestination = childSnapshot.val().destination;
-    var trainStarts = childSnapshot.val().primero;
-    var trainHowLongUntil = childSnapshot.val().soon;
+});
 
-    // train line information
-    console.log(trainName);
-    console.log(trainDestination);
-    console.log(trainStarts);
-    console.log(trainHowLongUntil);
 
-    // calculate the time for the next train
-    // frequency variable needs to be up at the top here.
-    var trainHowLongUntil = $("#howOften");
-    var preordained = moment(engineStart, "hh:mm").subtract(1, "years");
-    console.log(preordained);
-    // have computer recognise what time it is...
-    var currentTime = moment();
-    console.log("current time: " + moment(currentTime).format("hh:mm"));
-    //Difference between the times...
-    var diffTime = moment().diff(moment(preordained), "minutes");
-    console.log("difference in time: " + diffTime);
-    //how soon?
-    var timeLeft = diffTime % trainHowLongUntil;
-    console.log(timeLeft);
-    // 
-    var countDown = trainHowLongUntil - timeLeft;
-    console.log("minutes until train " + countDown);
-    
-    var nextTrain = moment().add(countDown, "minutes");
-    var nextTrainB = ("arrival time: " + moment(nextTrain).format('hh:mm'));
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
 
-    
+  var trainName = childSnapshot.val().name;
+  var trainDestination = childSnapshot.val().destination;
+  var trainStarts = childSnapshot.val().primero;
+  var trainHowLongUntil = childSnapshot.val().soon;
 
-    // create the train line
-    var newLine = $("<tr>").append(
-      $("<td>").text(trainName),
-      $("<td>").text(trainDestination),
-      $("<td>").text(nextOneComing),
-      $("<td>").text(nextTrainB),
-      $("<td>").text(trainHowLongUntil)
-    );
+  // train line information
+  console.log(trainName);
+  console.log(trainDestination);
+  console.log(trainStarts);
+  console.log(trainHowLongUntil);
 
-    $("#trainTable > tbody").append(newRow);
+  // calculate the time for the next train
+  // frequency variable needs to be up at the top here.
+  var trainNext = $("#howOften");
+  var preordained = moment(trainStarts, "hh:mm").subtract(1, "years");
+  console.log(preordained);
+  // have computer recognise what time it is...
+  var currentTime = moment();
+  console.log("current time: " + moment(currentTime).format("hh:mm"));
+  //Difference between the times...
+  var diffTime = moment().diff(moment(preordained), "minutes");
+  console.log("difference in time: " + diffTime);
+  //how soon add this to be stringified
+  var timeLeft = diffTime % trainHowLongUntil;
+  console.log(timeLeft);
+  // 
+  var countDown = trainHowLongUntil - timeLeft;
+  console.log("minutes until train " + countDown);
+  
+  var nextTrain = moment().add(countDown, "minutes");
+  var nextTrainB = ("arrival time: " + moment(nextTrain).format('hh:mm'));
 
-  })
+  
 
-})
+  // create the train line
+  var newLine = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(trainDestination),
+    $("<td>").text(trainHowLongUntil),
+    $("<td>").text(nextTrainB),
+    $("<td>").text(countDown)
+  );
+
+  $("#trainTable > tbody").append(newLine);
+
+});
