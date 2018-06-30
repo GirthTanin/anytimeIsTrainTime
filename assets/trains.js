790757103d64be6c40a3f2cf6879ad78334fa172
Jonathan McCarthy <JonathanFMcCarthy@gmail.com>
2018-06-30 02:46:55 -0400

    it's not done, but gahd fuhbid

---
+++ assets/trains.js
@@ -0,0 +1,14 @@
+// Firebase first
+<script src="https://www.gstatic.com/firebasejs/5.2.0/firebase.js"></script>
+
+var config = {
+  apiKey: "AIzaSyDaY_v5Av_DAEFUkeELOvs2xBWZsR8qAvw",
+  authDomain: "trains-rushjob.firebaseapp.com",
+  databaseURL: "https://trains-rushjob.firebaseio.com",
+  projectId: "trains-rushjob",
+  storageBucket: "",
+  messagingSenderId: "464928123166"
+};
+firebase.initializeApp(config);
+
+
